471760ba607f753dac529bca5977e0e0e5350432
arun <akant@xebia.com>
2020-02-11 18:55:02 +0530

    displayed first hero

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    heroes: [
+      {name: 'Geralt', power: 'Witcher'}
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        
+        <h1>{this.state.heroes[0].name}</h1>
+        <h3>{this.state.heroes[0].power}</h3>
       </div>
     );
   }
